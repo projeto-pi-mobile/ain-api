7cb4d13c1525cb6871202def541fe3b76c02fca0
Thiago <thiagotnon@gmail.com>
2021-06-03 23:40:57 -0300

    heroku deploy

--- config/database.js
+++ config/database.js
@@ -5,6 +5,8 @@ const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
+const URL = require('url-parse')
+const PROD_MYSQL_DB = new URL(Env.get("CLEARDB_DATABASE_URL"));
 
 module.exports = {
   /*
@@ -48,14 +50,16 @@ module.exports = {
   | npm i --save mysql
   |
   */
+
+
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', PROD_MYSQL_DB.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', PROD_MYSQL_DB.username),
+      password: Env.get('DB_PASSWORD', PROD_MYSQL_DB.password),
+      database: Env.get('DB_DATABASE', PROD_MYSQL_DB.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   },
