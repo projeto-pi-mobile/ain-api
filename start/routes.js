7ec2d70ac1861726850fe1807b9a55a82895ae46
Thiago <thiagotnon@gmail.com>
2021-06-09 14:27:22 -0300

    Exclude users without middlewares

--- start/routes.js
+++ start/routes.js
@@ -30,7 +30,7 @@ Route.post('/users', 'UserController.store').prefix('api/v1');
 Route.get('/users/:id', 'UserController.show').prefix('api/v1');
 Route.post('/users/token', 'UserController.token').prefix('api/v1');
 Route.put('/users/:id', 'UserController.update').middleware('auth').prefix('api/v1');
-Route.delete('/users/:id', 'UserController.destroy').middleware('auth').prefix('api/v1');
+Route.delete('/users/:id', 'UserController.destroy').prefix('api/v1');
 
 Route.get('/jobs', 'JobController.index').prefix('api/v1');
 Route.get('/jobs/:id', 'JobController.show').prefix('api/v1');
