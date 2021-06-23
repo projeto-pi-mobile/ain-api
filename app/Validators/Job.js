4d17addec06d94faf43a774edf5aed2e4f6e21d2
Thiago <thiagotnon@gmail.com>
2021-06-22 21:36:49 -0300

    Api

--- app/Validators/Job.js
+++ app/Validators/Job.js
@@ -7,7 +7,7 @@ class Job {
       description: 'required|max:400',
       hits: 'integer',
       user_id: 'integer|required',
-      category_id: 'integer|required',
+      category_id: 'integer',
 
     }
   }
